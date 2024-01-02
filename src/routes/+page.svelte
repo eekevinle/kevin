d508461b533e7732ad87b2f643da5f9f577a4284
Kevin Le <kevin.le@msasafety.com>
2024-01-01 20:51:04 -0500

    basic app

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,21 @@
+<script>
+	let count = 0;
+	const on_btn_blick = () => {
+		count++;
+	};
+</script>
+
 <!-- screen -->
 <div class="bg-black h-[100vh]">
 	<!-- main container -->
 	<div class="h-full flex flex-col justify-center place-items-center">
-		<div class="text-3xl font-black">kl.</div>
-		<div>Welcome to my site.</div>
+		<div class="text-9xl font-black">{count}</div>
+		<button
+			on:click={on_btn_blick}
+			class="bg-white font-bold text-black py-2 px-3
+             active:bg-neutral-900 active:text-white"
+		>
+			count++
+		</button>
 	</div>
 </div>
