9778094cd974678c0d7d6822f7c7989d138b0c23
Kevin Le <kevin.le@msasafety.com>
2024-01-01 21:07:57 -0500

    test bg scroll mobile

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,16 +6,9 @@
 </script>
 
 <!-- screen -->
-<div class="absolute inset-0 overflow-x-hidden w-full h-[100vh] bg-black">
-	<!-- content container -->
-	<div class="h-full flex flex-col justify-center place-items-center">
-		<div class="text-9xl font-black">{count}</div>
-		<button
-			on:click={on_btn_blick}
-			class="bg-white font-bold text-black py-2 px-3
-             active:bg-neutral-900 active:text-white"
-		>
-			count++
-		</button>
+<div class="bg-red-500 h-[100vh]">
+	<div class="font-bold">Hello!</div>
+	<div class="text-sm">
+		This is an example paragraph. It's a placeholder for whatever material I put here on the site.
 	</div>
 </div>
