c44a640445458c2e8b0ca61d2dbd24651657aab4
Kevin Le <kevin.le@msasafety.com>
2024-01-02 12:26:21 -0500

    intro section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -16,6 +16,21 @@
 
 	<div class="flex-1 overflow-y-auto pt-16 hide-scrollbar">
 		<div class="min-h-full flex flex-col justify-center items-center">
+			<!-- intro -->
+			<div class="h-96 flex flex-col gap-4 justify-center items-center">
+				<div class="text-3xl">Product Engineer @ <b>MSA Safety</b>.</div>
+				<div class="flex flex-col items-center">
+					<div class="max-w-md text-center">
+						Currently focusing on connected systems design and UX excellence for safety and
+						communications technologies.
+					</div>
+				</div>
+				<div class="text-sm text-neutral-500">
+					embedded | web | mobile | electronics | UX/UI | user research
+				</div>
+			</div>
+
+			<!-- sample sections -->
 			{#each sections as section}
 				<div
 					class="w-full flex justify-center items-center h-96
