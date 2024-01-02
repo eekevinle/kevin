ba7b2e608805d92d32ead01c9759a792e38c3efd
Kevin Le <kevin.le@msasafety.com>
2024-01-01 21:18:38 -0500

    test center margins

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,8 +8,9 @@
 <!-- screen -->
 <div class="absolute inset-0 flex overflow-x-hidden bg-black">
 	<!-- main container -->
-	<div class="">
+	<div class="bg-red-500 w-full m-8 flex flex-col justify-center items-center">
 		<div class="">{count}</div>
 		<button on:click={on_btn_blick}> count++ </button>
+		<div>Let's see if this orients correctly.</div>
 	</div>
 </div>
