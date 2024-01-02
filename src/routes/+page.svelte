ac064f70afae9444647d3c98daa2f85a2138c656
Kevin Le <kevin.le@msasafety.com>
2024-01-02 11:37:08 -0500

    bw

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,8 @@
 <script>
 	import Fab from '$lib/components/Fab.svelte';
 	import Navbar from '$lib/components/Navbar.svelte';
+
+	let num_sections = 4;
 </script>
 
 <div class="flex flex-col h-screen overflow-hidden bg-black">
@@ -8,10 +10,14 @@
 
 	<div class="flex-1 overflow-y-auto pt-16 hide-scrollbar">
 		<div class="min-h-full flex flex-col justify-center items-center">
-			<div class="w-full flex justify-center items-center h-96 bg-red-500">section</div>
-			<div class="w-full flex justify-center items-center h-96 bg-orange-500">section</div>
-			<div class="w-full flex justify-center items-center h-96 bg-green-500">section</div>
-			<div class="w-full flex justify-center items-center h-96 bg-blue-500">section</div>
+			{#each { length: num_sections } as _, i}
+				<div
+					class="w-full flex justify-center items-center h-96
+					 			 border-b-[1px] border-neutral-800 text-neutral-800"
+				>
+					section {i}
+				</div>
+			{/each}
 		</div>
 	</div>
 
