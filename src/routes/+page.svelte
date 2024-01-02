ab1d1d9e9509edcc985d57ec2dc774541bca948d
Kevin Le <kevin.le@msasafety.com>
2024-01-01 20:32:49 -0500

    basic tailwind usage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,8 @@
-<div class="text-green-500">kl.</div>
+<!-- screen -->
+<div class="bg-black h-[100vh]">
+	<!-- main container -->
+	<div class="h-full flex flex-col justify-center place-items-center">
+		<div class="text-3xl font-black">kl.</div>
+		<div>Welcome to my site.</div>
+	</div>
+</div>
