88aa416c9124efa2c2c4e01e95795e98fdc2a44c
Kevin Le <kevin.le@msasafety.com>
2024-01-01 21:13:58 -0500

    absolute inset-0

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,9 +6,10 @@
 </script>
 
 <!-- screen -->
-<div class="bg-red-500 h-[100vh]">
-	<div class="font-bold">Hello!</div>
-	<div class="text-sm">
-		This is an example paragraph. It's a placeholder for whatever material I put here on the site.
+<div class="absolute inset-0 flex overflow-x-hidden bg-black">
+	<!-- main container -->
+	<div class="">
+		<div class="">{count}</div>
+		<button on:click={on_btn_blick}> count++ </button>
 	</div>
 </div>
